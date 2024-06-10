e0c3a3a6f4d7fa57c6ea014f225bdd68c0b94777
Antonio Perez <antonio.perez@jala.university>
2024-06-10 09:31:07 -0600

    Connection-correction

--- DataBase/Connection.php
+++ DataBase/Connection.php
@@ -1,33 +1,34 @@
 <?php
-class Database{
+class Database {
     private $connection;
-    public function __construct()
-    {
-        
+
+    public function __construct() {
+        // Constructor vacío
     }
-    public function verificarDriver(){
-        $miArray = (PDO::getAvailableDrivers());
-        $encontrado = false;
-        foreach($miArray as $n){
-            if ($n=='mysql') {
-                $encontrado = true;
-                break;
-            }
-        }
-        return $encontrado;
+
+    public function verificarDriver() {
+        $miArray = PDO::getAvailableDrivers();
+        return in_array('mysql', $miArray);
     }
-    public function getConnection(){
+
+    public function getConnection() {
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
-        $dsn = "mysql:host=localhost;dbname=progweb";
+        $dsn = "mysql:host=roundhouse.proxy.rlwy.net;port=11643;dbname=railway";
         $user = "root";
-        $password = "";
-        $this->connection = new PDO($dsn,$user,$password,$options);
-        $this->connection->exec("SET CHARACTER SET UTF8");
+        $password = "gbKMxgNyjUPfXFIKSxgXxeyoKxePXYxJ";
+        
+        try {
+            $this->connection = new PDO($dsn, $user, $password, $options);
+            $this->connection->exec("SET CHARACTER SET UTF8");
+        } catch (PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+
         return $this->connection;
     }
 }
-?>
+
